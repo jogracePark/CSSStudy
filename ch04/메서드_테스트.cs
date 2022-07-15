b58f3bae5bac48021e34e337550cec0385785726
unknown <you@example.com>
2022-07-15 15:01:39 +0900

    2022/07/15 ch04 수 업 예 제

---
+++ ch04/메서드_테스트.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch04
+{
+    internal class 메서드_테스트
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("학원예시 작업");
+        }
+    }
+}
